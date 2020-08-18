e641356fd32a1909899965985a928c06c4fc1007
Cullen O'Hara <Cullen.OHara@outlook.com>
2020-08-18 14:23:39 -0400

    Update outer_model.py

--- src/models/outer_model.py
+++ src/models/outer_model.py
@@ -69,10 +69,10 @@ class outer_model:
         self.X1 = intermediate_layer_model.predict(self.X1, batch_size=self.params[0]["batch_size"])
         self.X2 = intermediate_layer_model.predict(self.X2, batch_size=self.params[0]["batch_size"])
 
-        ten_auth_mask = np.argwhere(np.isin(self.y1, list(range(10))))
+        ten_auth_mask = np.squeeze(np.argwhere(np.isin(self.y1, list(range(10)))))
         print(ten_auth_mask.shape, flush=True)
-        masked_authors = self.X1[ten_auth_mask[0]]
-        masked_labels = self.y1[ten_auth_mask[0]]
+        masked_authors = self.X1[ten_auth_mask.astype(dtype=np.int16)]
+        masked_labels = self.y1[ten_auth_mask(dtype=np.int16)]
 
 
         print("X1 shape: ", np.array(self.X1).shape, flush=True)
